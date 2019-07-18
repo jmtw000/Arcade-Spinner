3518aabe3b4faf0ec381302c88be99dd67eba330
jmtw000 <46089724+jmtw000@users.noreply.github.com>
2019-07-18 12:58:09 -0400

    Update Arcade_Spinner.ino

--- Arcade_Spinner.ino
+++ Arcade_Spinner.ino
@@ -1,4 +1,4 @@
-/*   Arcade Spinner v0.6
+/*   Arcade Spinner v0.7
 *    Copyright 2018 Joe W (jmtw000 a/t gmail.com)
 *                   Craig B - Updated code for mouse movement modes(DROP, ACCM) and case statement for Button port bit validation
 *    
@@ -19,13 +19,7 @@
 #include "Mouse.h"
 #include <Joystick.h>
 
-//Mouse movement modes - pick one to compile specific code
-//#define NORM   //Mouse movement Normal; every movement processed; may lag in fast movement
-//#define DROP   //Mouse movement Drop; drop extra movements; smooth movement, no lag
-#define ACCM   //Mouse movement Accelerated; the faster you turn the encoder the larger the individual mouse movements, no lag
-//#define STATE    //Mouse movement STATE; A state variable (1=right, 2=left) is set at each interrupt depending on the direction the encoder is turning and the mouse is moved in loop() based on that. No lag.
                  
-
 #define pinA 2    //The pins that the rotary encoder's A and B terminals are connected to.
 #define pinB 3
 #define maxBut 6  //The number of buttons you are using up to 10.
@@ -34,7 +28,7 @@
 //Create a Joystick object.
 Joystick_ Joystick(JOYSTICK_DEFAULT_REPORT_ID,JOYSTICK_TYPE_GAMEPAD,
   maxBut, 0,             // Button Count, Hat Switch Count
-  true, false, false,    // X, but no Y or Z Axis. We need at least one axis even though it's not really used.
+  true, true, false,    // X, Y, but no Z Axis. We need at least two axes even though they're used.
   false, false, false,   // No Rx, Ry, or Rz
   false, false,          // No rudder or throttle
   false, false, false);  // No accelerator, brake, or steering;
@@ -43,16 +37,11 @@ Joystick_ Joystick(JOYSTICK_DEFAULT_REPORT_ID,JOYSTICK_TYPE_GAMEPAD,
 //The previous state of the AB pins
 volatile int previousReading = 0;
 
-#ifndef STATE //not needed in STATE mode
 //Keeps track of how much the encoder has been moved
 volatile int rotPosition = 0;
-#endif
 
-#if !defined(STATE) || !defined(NORM) //not needed in STATE or NORM modes
 volatile int rotMulti = 0;
-#endif
 
-volatile int state = 0; //0=no movement, 1=right, 2=left
 
 //Set the initial last state of the buttons depending on the max number of buttons defined in maxBut
 
@@ -109,9 +98,10 @@ void setup() {
   //Start the joystick
   Joystick.begin();
   
-  //Center the X axis on the joystick
+  //Center the X and Y axes on the joystick
   Joystick.setXAxis(511);
-
+  Joystick.setYAxis(511);
+  
   //Set up the interrupt handler for the encoder's A and B terminals on digital pins 2 and 3 respectively. Both interrupts use the same handler.
   attachInterrupt(digitalPinToInterrupt(pinA), pinChange, CHANGE); 
   attachInterrupt(digitalPinToInterrupt(pinB), pinChange, CHANGE);
@@ -141,13 +131,8 @@ void pinChange() {
      combinedReading == 0b1101 || 
      combinedReading == 0b0100) {
      
-     #ifndef STATE
      rotPosition++;                   //update the position of the encoder
-     #endif
 
-     #ifdef STATE
-     state=1;
-     #endif
   }
 
   //Going to the left
@@ -155,13 +140,9 @@ void pinChange() {
      combinedReading == 0b0111 ||
      combinedReading == 0b1110 ||
      combinedReading == 0b1000) {
-     #ifndef STATE
+     
      rotPosition--;                   //update the position of the encoder
-     #endif
-
-     #ifdef STATE
-     state=2;
-     #endif
+     
   }
 
 
@@ -171,56 +152,22 @@ void pinChange() {
 
 
 void loop(){ 
+
   int currentButtonState;
-//int rotMulti;
 
-#ifdef ACCM
-  //If the encoder has moved 2 or more transitions move the mouse in the appropriate direction 
-  //and update the position variable to reflect that we moved the mouse. Accelerated move.
-  if(rotPosition >= 2 || rotPosition <= -2) {
-    rotMulti = rotPosition;// >> 1;
+
+  //If the encoder has moved 1 or more transitions move the mouse in the appropriate direction 
+  //and update the rotPosition variable to reflect that we have moved the mouse. The mouse will move 1/2 
+  //the number of pixels of the value currently in the rotPosition variable. We are using 1/2 (rotPosition>>1) because the total number 
+  //of transitions(positions) on our encoder is 2400 which is way too high. 1200 positions is more than enough.
+  
+  if(rotPosition >= 1 || rotPosition <= -1) {
+    rotMulti = rotPosition>> 1;                 //copy rotPosition/2 to a temporary variable in case there's an interrupt while we're moving the mouse 
     Mouse.move(rotMulti,0,0);
-    rotPosition -= (rotMulti);// << 1);
+    rotPosition -= (rotMulti<< 1);              //adjust rotPosition to account for mouse movement
   }
-#endif 
 
-#ifdef DROP
-  //If the encoder has moved 2 or more transitions move the mouse in the appropriate direction 
-  //and update the position variable to reflect that we moved the mouse. Smooth drop extra moves.
-  if(rotPosition >= 2) {
-    Mouse.move(1,0,0);
-    rotPosition = 0;
-  } else if(rotPosition <= -2) {
-    Mouse.move(-1,0,0);
-    rotPosition = 0;
-  } 
-#endif 
 
-#ifdef NORM
-  //If the encoder has moved 2 or more transitions move the mouse in the appropriate direction 
-  //and update the position variable to reflect that we moved the mouse. Smooth movement but lags.
-  if(rotPosition >= 2) {
-    Mouse.move(1,0,0);
-    rotPosition -= 2;
-  } else if(rotPosition <= -2) {
-    Mouse.move(-1,0,0);
-    rotPosition += 2;
-  } 
-#endif 
-
-#ifdef STATE
-switch (state){
-  case 0:
-    break;
-  case 1:
-    Mouse.move(1,0,0);
-    break;
-  case 2:
-    Mouse.move(-1,0,0);
-    break;
-}
-state=0;
-#endif
 
   //Iterate through the 10 buttons (0-9) assigning the current state of the pin for each button, HIGH(0b00000001) or LOW(0b00000000), to the currentState variable
   int button = 0;
